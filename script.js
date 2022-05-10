a67fabdc560b1f788047f1e2ac366113cd8309f3
Clement <perso@clementhurel.fr>
2022-05-10 22:19:57 +0900

    tic-tac-toe update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let boardArray = [];
+const gameBoard = (() => {
 
 
-
-
-//Make player constructor for player object or FACTORY
-const player = () => {
-      return{
-            player : 1, 
-            signMarker : "X",
-      }
+// Create player
+const playerCreation = (name, marker, bot) => {
+      return{ name, marker, bot}
 };
 
+// Game setup
+let boardArray = [];
+let winner = null;
+let winPossibilities = [[0, 1, 2],
+                        [3, 4, 5],
+                        [6, 7, 8],
+                        [0, 3, 6],
+                        [1, 4, 7],
+                        [2, 5, 8],
+                        [0, 4, 8],
+                        [2, 4, 6]];
 
 
-
-const xMarkerBtn = document.getElementsByClassName("xMarker");
-const oMarkerBtn = document.getElementsByClassName("oMarker");
-let playerMarker;
-
-xMarkerBtn.addEventListener("click",console.log(34));
 
 gameplaySelectionMod();  // User choose between 1VBot or 1V1
 markerSelectionMod()     // Player 1 choose a sign between X and 0;
 
-// Modules 1VBot
+
 currentPlayerMod();      // Checks which player is playing now (Player 1 or Player 2, Player 1 or Bot)
 gameMoveMod();           // Player select a square, actions occur (render, taken spots, updategameboard display, result...)
 takenSpotsCheckMod();    // Check if player selection is really empty or already taken (+ list of all taken + list of all available)
@@ -35,14 +35,6 @@ resultMod();             // Check current play and compare to winPossibilities,
 restartMod();            // Restart the game
 
 
+});
 
 
-
-let winPossibilities = [[0, 1, 2],
-                        [3, 4, 5],
-                        [6, 7, 8],
-                        [0, 3, 6],
-                        [1, 4, 7],
-                        [2, 5, 8],
-                        [0, 4, 8],
-                        [2, 4, 6]];
