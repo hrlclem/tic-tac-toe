46db3771c42b97d1682a4a85616340b5f4d9ca8c
Clement <perso@clementhurel.fr>
2022-05-10 21:01:36 +0900

    tic-tac-toe update

--- script.js
+++ script.js
@@ -14,19 +14,30 @@ console.log(player("X"));
 
 // Modules
 const markerSelectionMod = (() => {
-      
+      let playerMarker;
+      const xMarkerBtn = document.getElementsByClassName("xMarker");
+      const oMarkerBtn = document.getElementsByClassName("oMarker");
+      xMarkerBtn.onclick = function(){
+            playerMarker = "x";
+      }
+      oMarkerBtn.onclick = function(){
+            playerMarker = "o";
+      }
+      console.log(playerMarker)
+      return playerMarker;
 })();    // Choose a sign between X and 0;
-gameplayMod();           // Player select a square, actions occur (render, result...)
-updateGameboardMod();    // Update and display choice on gameboard
-currentPlayerMod();      // Checks which player is playing now
-takenSpotsCheckMod();    // Check if player selection is really empty or already taken
-renderMod();             // Roles the play
-resultMod();             // Check current play and compare to winPossibilities, 
-                              // gives result (player win or tie), popup appears and offer to restart
-restartMod();            // Restart the game
+
+// gameplayMod();           // Player select a square, actions occur (render, result...)
+// updateGameboardMod();    // Update and display choice on gameboard
+// currentPlayerMod();      // Checks which player is playing now
+// takenSpotsCheckMod();    // Check if player selection is really empty or already taken
+// renderMod();             // Roles the play
+// resultMod();             // Check current play and compare to winPossibilities, 
+//                               // gives result (player win or tie), popup appears and offer to restart
+// restartMod();            // Restart the game
 
 
-
+// console.log(playerMarker + 33)
 
 
 
